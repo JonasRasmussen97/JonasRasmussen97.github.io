053f9386f1c3fc19f3bdc7f50c18eef8403777fd
Jonas <Saltogpeber123@hotmail.com>
2019-07-11 18:59:15 +0200

    Updates

---
+++ precache-manifest.45824f00ad2633c1f734190f6f54d392.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d893a8b91d9f6f0990b5595f0e82c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d23f4bd5ab9cee8289d",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "1677cd59de19cba05c95",
+    "url": "/static/css/main.568c60eb.chunk.css"
+  },
+  {
+    "revision": "5d23f4bd5ab9cee8289d",
+    "url": "/static/js/2.0ac7d4b1.chunk.js"
+  },
+  {
+    "revision": "1677cd59de19cba05c95",
+    "url": "/static/js/main.2bf20d07.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
